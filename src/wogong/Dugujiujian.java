1fc0b0aaf89af5f5016ed0a39f5cac7528126c06
木样 <oopsn@foxmail.com>
2019-11-12 20:59:30 +0800

    fsdf

--- src/wogong/Dugujiujian.java
+++ src/wogong/Dugujiujian.java
@@ -4,6 +4,7 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
     System.out.println("fewfaefa ");
+    System.out.println("fewfaefa ");
     System.out.println("fewf鸡同鸭讲 ");
 	}
 
